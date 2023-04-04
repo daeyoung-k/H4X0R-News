07f316aab39a4e7dbc92b5d032da0b14acd1d84c
kkwon_dae <eodud4976@gmail.com>
2023-04-04 23:11:51 +0900

    Initial Commit

---
+++ H4X0R News/H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NewsApp.swift
+//  H4X0R News
+//
+//  Created by 권대영 on 2023/04/04.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
